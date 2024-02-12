a843982e12774724859644e0ccd4c64cfba07663
Marek Nowicki <faramir2@gmail.com>
2024-02-12 18:10:32 +0100

    First HPV search in Java using Tversky

---
+++ src/main/java/pl/edu/icm/heap/HpvViruses.java
@@ -0,0 +1,73 @@
+package pl.edu.icm.heap;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.util.*;
+
+public class HpvViruses {
+    private final List<String> hpvNames;
+    private final Map<String, Set<String>> hpvViruses;
+    private final Set<String> superset;
+
+    public HpvViruses() throws IOException {
+        hpvNames = new ArrayList<>();
+        hpvViruses = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(
+                        Objects.requireNonNull(
+                                HpvViruses.class.getResourceAsStream("/hpv_viruses.fasta"))))) {
+            String name = "";
+            StringBuilder virus = new StringBuilder();
+            while (true) {
+                String line = br.readLine();
+                if (line == null || line.startsWith(">")) {
+                    if (!virus.isEmpty()) {
+                        Set<String> generator = new HashSet<>();
+                        for (int i = 0; i < virus.length() - Main.SHINGLETON_LENGTH; ++i) {
+                            generator.add(virus.substring(i, i + Main.SHINGLETON_LENGTH));
+                        }
+                        hpvNames.add(name);
+                        hpvViruses.put(name, Collections.unmodifiableSet(generator));
+
+                        virus.setLength(0);
+                    }
+                    if (line == null) {
+                        break;
+                    }
+
+                    int index = line.indexOf("|");
+                    name = line.substring(1, index == -1 ? line.length() : index);
+                } else {
+                    virus.append(line);
+                }
+            }
+        }
+
+        superset = new HashSet<>();
+        hpvViruses.values().forEach(superset::addAll);
+    }
+
+    public boolean hasShinglet(String shinglet) {
+        return superset.contains(shinglet);
+    }
+
+    public PriorityQueue<CrosscheckResult> crosscheck(Set<String> shinglets) {
+        PriorityQueue<CrosscheckResult> priorityQueue = new PriorityQueue<>((v1, v2) -> {
+            int value = Double.compare(v1.value(), v2.value());
+            if (value == 0) return v1.name().compareTo(v2.name());
+            return -value;
+        });
+        for (String hpvName : hpvNames) {
+            Set<String> hpvShinglets = hpvViruses.get(hpvName);
+            long intersectionSize = shinglets.stream().filter(hpvShinglets::contains).count();
+            double index =  (double) intersectionSize / hpvShinglets.size();
+            priorityQueue.add(new CrosscheckResult(hpvName, index));
+        }
+        return priorityQueue;
+    }
+
+    public record CrosscheckResult(String name, double value) {
+    }
+}
--- src/main/java/pl/edu/icm/heap/Main.java
+++ src/main/java/pl/edu/icm/heap/Main.java
@@ -1,67 +1,85 @@
 package pl.edu.icm.heap;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.zip.GZIPInputStream;
 
 public class Main {
-    private static final int shingletonLength = 18;
+    public static final int SHINGLETON_LENGTH = Integer.parseInt(System.getProperty("shingletonLength", "18"));
 
     public static void main(String[] args) throws IOException {
-        String filename = "sample_08414.fq.gz";
+        Instant startTime = Instant.now();
 
-        Instant start = Instant.now();
-        int count = 0;
+        System.err.print("Reading HPV virus file...");
+        System.err.flush();
 
-        Set<String> shinglets = new HashSet<>();
-        try (BufferedReader input = new BufferedReader(new InputStreamReader(
-//                new BufferedInputStream(
-//                        new FileInputStream(filename)),
-                new GZIPInputStream(new FileInputStream(filename), 16 * 1024 * 1024)), 32 * 1024 * 1024)) {
+        HpvViruses hpvViruses = new HpvViruses();
+        System.err.printf(" takes %.6f\n", Duration.between(startTime, Instant.now()).toNanos() / 1e9);
 
-            String line;
-            input.readLine(); // skip line
+        for (String filename : args) {
+            try (BufferedReader input = new BufferedReader(
+                    new InputStreamReader(
+                            new GZIPInputStream(
+                                    new FileInputStream(filename), 16 * 1024 * 1024)), 32 * 1024 * 1024)) {
+                Instant readingTime = Instant.now();
+                System.err.println("Processing '" + filename + "' file...");
 
-            StringBuilder sb = new StringBuilder();
+                System.err.print("\treading... ");
+                System.err.flush();
 
-            char[] sample = new char[shingletonLength * 100_000];
-            while ((line = input.readLine()) != null) {
-                sb.append(line);
-                while (sb.length() >= sample.length) {
-                    sb.getChars(0, sample.length, sample, 0);
-                    for (int i = 0; i < sample.length - shingletonLength; ++i) {
-                        String shinglet = new String(sample, i, shingletonLength);
-//                        shinglets.add(shinglet);
-                        count += shinglet.length();
+                Set<String> shinglets = new HashSet<>();
+                input.readLine(); // skip line
+                StringBuilder sb = new StringBuilder();
+                while (true) {
+                    String line = input.readLine();
+                    if (line != null) {
+                        sb.append(line);
                     }
-                    sb.delete(0, sample.length - shingletonLength);
+                    if (line == null || sb.length() >= 16 * 1024 * 1024) {
+                        for (int i = 0; i <= sb.length() - SHINGLETON_LENGTH; ++i) {
+                            String shinglet = sb.substring(i, i + SHINGLETON_LENGTH);
+                            if (hpvViruses.hasShinglet(shinglet)) {
+                                shinglets.add(shinglet);
+                            }
+                        }
+                        if (line == null) {
+                            break;
+                        }
+                        sb.delete(0, sb.length() - SHINGLETON_LENGTH + 1);
+                    }
+                    input.readLine(); // skip line
+                    input.readLine(); // skip line
+                    input.readLine(); // skip line
                 }
-                input.readLine(); // skip line
-                input.readLine(); // skip line
-                input.readLine(); // skip line
-            }
-            sample = new char[sb.length()];
-            sb.getChars(0, sample.length, sample, 0);
-            for (int i = 0; i < sample.length - shingletonLength; ++i) {
-                String shinglet = new String(sample, i, shingletonLength);
-//                shinglets.add(shinglet);
-                count += shinglet.length();
-            }
+                System.err.printf(" takes %.9f%n", Duration.between(readingTime, Instant.now()).toNanos() / 1e9);
 
+
+                System.err.print("\tcrosscheck... ");
+                System.err.flush();
+                Instant crosscheckTime = Instant.now();
+
+                StringBuilder result = new StringBuilder();
+                PriorityQueue<HpvViruses.CrosscheckResult> resultsPQ = hpvViruses.crosscheck(shinglets);
+                for (int i = 0; i < 3; ++i) {
+                    HpvViruses.CrosscheckResult max =resultsPQ.poll();
+                    if (max == null) {
+                        break;
+                    }
+                    result.append(String.format("%-10s\t%.6f\t", max.name(), max.value()));
+                }
+                System.err.printf(" takes %.9f%n", Duration.between(crosscheckTime, Instant.now()).toNanos() / 1e9);
+                System.out.printf("%s%s%n", result, filename);
+            }
         }
-        System.out.println("Size: " + count);
-        Instant finish = Instant.now();
 
-        long timeElapsed = Duration.between(start, finish).toMillis();
-        System.out.println("Time: " + timeElapsed / 1e3);
+        long timeElapsed = Duration.between(startTime, Instant.now()).toNanos();
+        System.out.printf("Total time: %.9f%n", timeElapsed / 1e9);
     }
 }
